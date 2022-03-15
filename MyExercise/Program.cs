1b485a42bde47cbc860fa5ab445c09cb322de06c
user <user@DESKTOP-2JQTQPV>
2022-03-16 02:11:14 +0400

    Add project files.

---
+++ MyExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Services services = new Services();
+
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6, 23 };
+
+            string[] names = new string[] { "Gocha", "Gio", "Levani" };
+
+            //services.Writer(names);
+
+            List<int> list = services.RemoveFromList(numbers, 23); //Test Remove Element from List
+
+            services.Writer(list);
+
+            
+
+        }
+
+    }
+}
---
+++ MyExercise/Services.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExercise
+{
+    internal class Services
+    {
+        #region Write Elements of List
+        public void Writer<T>(IEnumerable<T> ar)
+        {
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        #endregion
+
+        #region Remove item From List and return
+        public List<T> RemoveFromList<T>(List<T> list, T item)
+        {
+            list.Remove(item);
+            
+            return list;
+        }
+        #endregion
+
+    }
+}
